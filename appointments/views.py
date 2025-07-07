bafdb00a458d8e327bd8445a97bca7495dcc2e68
bafodej <bafode.jaiteh4@gmail.com>
2025-07-07 19:49:44 +0200

    add coach and users dashboard

--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
 
 def accueil(request):
     return render(request, 'appointments/accueil.html')
 
 def index(request):
     return render(request, 'appointments/accueil.html')
+
+
+
+def signup(request):
+    """
+    Formulaire d'inscription de la page singin,
+    renvoi vers le dashboard ou vers un nouveau formuaire 
+    """
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def logout_view(request):  # ← Ajoutez cette fonction
+    logout(request)
+    return redirect('accueil')
+
+@login_required
+def dashboard(request):
+    if request.user.is_superuser:
+        # Coach dashboard
+        return render(request, 'appointments/dashboard_coach.html')
+    else:
+        # Client dashboard  
+        return render(request, 'appointments/dashboard_client.html')
