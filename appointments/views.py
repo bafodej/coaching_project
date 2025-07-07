fdd5593e66601f752a7daaeb1206c4b596fff5b1
bafodej <bafode.jaiteh4@gmail.com>
2025-07-07 11:33:09 +0200

    add index.html +base.html

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def accueil(request):
+    return render(request, 'appointments/accueil.html')
+
+def index(request):
+    return render(request, 'appointments/accueil.html')
